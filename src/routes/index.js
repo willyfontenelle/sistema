00eeae0b4e0d3cf82540b723ec5156efb3527c3d
willyfontenelle <willyfontenelle@gmail.com>
2023-03-30 19:57:53 -0300

    fix package json x react-router-dom

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard'
+import { Switch } from 'react-router-dom';
 
-function RoutesApp(){
-  return(
+function RoutesApp() {
+  return (
     <Routes>
-      <Route path="/" element={ <SignIn/> } />
-      <Route path="/register" element={ <SignUp/> } />
-      <Route path="/dashboard" element={ <Dashboard/> } />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
