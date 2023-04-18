6b1a3a706c665ee4ec385651e65237e018024056
willyfontenelle <willyfontenelle@gmail.com>
2023-04-18 18:05:57 -0300

    fim aula 105

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard'
 import { Switch } from 'react-router-dom';
 
+import Private from './Private';
+
 function RoutesApp() {
   return (
     <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={ <Private><Dashboard/></Private>} />
     </Routes>
   )
 }
