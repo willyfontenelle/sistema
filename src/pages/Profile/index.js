84338b10115982fd083c5004e51c7adbf8c165c2
willyfontenelle <willyfontenelle@gmail.com>
2023-04-25 06:25:21 -0300

    25/04 06:25

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,82 @@
+import { useContext, useState } from 'react';
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiSettings, FiUpload } from 'react-icons/fi';
+import avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../contexts/auth'
+
+import './profile.css';
+
+export default function Profile(){
+
+    const { user, storageUser, setUser, logout } = useContext(AuthContext);
+
+    
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    const [nome, setNome] = useState(user && user.nome)
+    const [email, setEmail] = useState(user && user.email)
+
+    function handleFile(e){
+        if(e.target.files[0]){
+            const image = e.target.files[0];
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png'){
+                setImageAvatar(image)
+                setAvatarUrl(URL.createObjectURL(image))
+            }else{
+                alert("Envie uma imagem do tipo PNG ou JPEG")
+                setImageAvatar(null);
+                return;
+            }
+        }
+    }
+
+
+    return(
+        <div>
+            <Header/>
+            
+            <div className="content">
+                <Title name="Minha conta">
+                    <FiSettings size={25}/>
+                </Title>
+
+                <div className="container">
+                    <form className="form-profile">
+                        <label className="label-avatar">
+                            <span>
+                                <FiUpload color="#fff" size={25}/>
+                            </span>
+                            <input type="file" accept="image/*" onChange={handleFile}/><br/>
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt="Foto de perfil" width={250} height={250}/>
+                            ) : (
+                                <img src={avatar} alt="Foto de perfil" width={250} height={250}/>
+                            )}
+
+                        </label>
+
+                        <label>Nome</label>
+                        <input type="text" value={nome} onChange={(e) => setNome(e.target.valuye)}/>
+
+                        <label>Email</label>
+                        <input type="text" value={email} disabled={true}/>
+
+                        <button type="submit">Salvar</button>
+
+                    </form>
+
+                </div>
+
+                <div className="container">
+                    <button className="logout-btn" onClick={ () => logout() }>Sair</button>
+                </div>
+
+            </div>
+
+            
+        </div>
+    )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard'
-import { Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 import Private from './Private';
 
@@ -13,6 +13,7 @@ function RoutesApp() {
         <Route path="/" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
         <Route path="/dashboard" element={ <Private><Dashboard/></Private>} />
+        <Route path="/profile" element={ <Private><Profile/></Private>}/>
     </Routes>
   )
 }
