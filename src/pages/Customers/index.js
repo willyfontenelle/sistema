392891c7e7934ea3e8a01bf8c560f8effd04c64a
willyfontenelle <willyfontenelle@gmail.com>
2023-04-28 15:55:48 -0300

    fim aula 113

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -4,16 +4,39 @@ import Title from '../../components/Title';
 
 import { FiUser } from 'react-icons/fi'
 
+import { db } from '../../services/firebaseConnection'
+import { addDoc, collection } from 'firebase/firestore'
+
+import { toast } from 'react-toastify'
+
 export default function Customers() {
 
     const [nome, setNome] = useState('')
     const [cnpj, setCnpj] = useState('')
     const [endereco, setEndereco] = useState('')
 
-    function handleRegister(e){
+    async function handleRegister(e){
         e.preventDefault();
 
-        alert("TESTE")
+        if(nome !== '' && cnpj !== '' && endereco !== ''){
+            await addDoc(collection(db, "customers"), {
+                nomeFantasia: nome,
+                cnpj: cnpj,
+                endereco: endereco
+            })
+            .then(()=> {
+                setNome('')
+                setCnpj('')
+                setEndereco('')
+                toast.success("Empresa registrada!")
+            })
+            .catch((error)=>{
+                console.log(error);
+                toast.error("Erro ao fazer o cadastro.")
+            })
+        }else{
+            toast.error("Preencha todos os campos!")
+        }
     }
 
     return (
